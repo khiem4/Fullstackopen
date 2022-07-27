30861f2f806774a369a64f912dfe86b285abf1e1
khiem4 <98609753+khiem4@users.noreply.github.com>
2022-07-27 20:51:09 +0700

    exercise 2.11

--- part2/Phonebook/App.js
+++ part2/Phonebook/App.js
@@ -1,16 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setFilter] = useState('')
-  const [personsToShow, setPersonToShow] = useState(persons)
+  const [personsToShow, setPersonsToShow] = useState(persons)
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+        setPersonsToShow(response.data)
+      })
+  }, [])
+
 
   const addName = (event) => {
     event.preventDefault()
@@ -24,15 +32,16 @@ const App = () => {
       return alert(`${newName} ${newNumber} is already added to phonebook`)
     } else {
       setPersons(persons.concat(nameNumberObject))
-      setPersonToShow(persons.concat(nameNumberObject))
+      setPersonsToShow(persons.concat(nameNumberObject))
       setNewName('')
       setNewNumber('')
     }
   }
+
   const filterName = (event) => {
     const filter = event.target.value
     setFilter(filter)
-    setPersonToShow(persons.filter(person =>
+    setPersonsToShow(persons.filter(person =>
       person.name.toLowerCase().includes(newFilter.toLocaleLowerCase()
       )))
   }
@@ -49,7 +58,8 @@ const App = () => {
       <h2>Phone book</h2>
       <Filter newFilter={newFilter} filterName={filterName} />
       <h2>Add a new</h2>
-      <PersonForm addName={addName} newName={newName} handleNameChange={handleNameChange}
+      <PersonForm addName={addName}
+        newName={newName} handleNameChange={handleNameChange}
         newNumber={newNumber} handleNumberChange={handleNumberChange} />
       <h2>Numbers</h2>
       <Persons personsToShow={personsToShow} />
@@ -65,10 +75,10 @@ const Filter = ({ newFilter, filterName }) => {
   )
 }
 
-const Persons = (props) => {
+const Persons = ({ personsToShow }) => {
   return (
     <>
-      {props.personsToShow.map(person =>
+      {personsToShow.map(person =>
         <p key={person.id}> {person.name} {person.number}</p>)}
     </>
   )
@@ -94,9 +104,5 @@ const PersonForm = (props) => {
     </form>
   )
 }
-
-
-
-
 
 export default App
