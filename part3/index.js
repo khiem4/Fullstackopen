1238a272e3daf1560432137063e81a95b6ffa336
khiem4 <khiemcan@gmail.com>
2022-08-13 22:02:41 +0700

    exercise 3.4

--- part3/index.js
+++ part3/index.js
@@ -1,33 +1,30 @@
-const express = require('express')
+const express = require("express")
 const app = express()
 
 app.use(express.json())
 
-
-const persons = [
+let persons = [
     {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
+        name: "Arto Hellas",
+        number: "040-123456",
+        id: 1,
     },
     {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+        id: 2,
     },
     {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
+        name: "Dan Abramov",
+        number: "12-43-234345",
+        id: 3,
     },
     {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
+        name: "Mary Poppendieck",
+        number: "39-23-6423122",
+        id: 4,
+    },
 ]
-
-
 app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
@@ -47,7 +44,15 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 
+app.delete("/api/persons/:id", (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter((person) => person.id !== id)
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+    response.status(204).end()
+})
+
+
+const PORT = 3002
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
