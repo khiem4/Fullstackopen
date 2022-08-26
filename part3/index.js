2468378886ceebf8b7ae19de1d5388b26f6d0f56
khiem4 <khiemcan@gmail.com>
2022-08-27 00:53:31 +0700

    exercise 3.17-3.18

--- part3/index.js
+++ part3/index.js
@@ -4,7 +4,7 @@ const express = require("express")
 const app = express()
 const morgan = require("morgan")
 const cors = require('cors')
-const Person = require('./models/phonebook')
+const Persons = require('./models/phonebook')
 const mongoose = require('mongoose')
 
 
@@ -26,69 +26,79 @@ app.use(morgan(`
 
 
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(person => {
+    const get = Persons.find({}).then(person => {
         response.json(person)
     })
 })
 
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
-    const newPerson = new Person({
+
+    const newPerson = new Persons({
         name: body.name,
         number: body.number,
         date: new Date()
     })
 
-    // const checkName = persons.find(p => p.name === newPerson.name)
-
-    // if (checkName) {
-    //     return response.status(400).json({
-    //         error: 'name must be unique'
-    //     })
-    // } else if (!newPerson.name || !newPerson.number) {
-    //     return response.status(400).json({
-    //         error: 'name or number is missing'
-    //     })
-    // }
     newPerson.save()
-        .then(savedPerson => {
-            response.json(savedPerson)
+        .then(result => response.json(result))
+        .catch(error => next(error))
+})
+
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Persons.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
         })
         .catch(error => next(error))
 })
 
 
 app.get('/info', (request, response) => {
-    response.send
-        (`Phonebook has info for ${persons.length} people <br/>${Date()}`)
-})
-
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(p => p.id === id)
-    if (person) {
-        response.json(person)
-    }
-    response.status(404).end()
+    Persons.find({})
+        .then(result => response.send(`Phonebook has info for ${result.length} people <br/>${Date()}`))
+        .catch(error => next(error))
 })
 
 
-app.delete("/api/persons/:id", (request, response) => {
-    Person.findByIdAndRemove(request.params.id)
+
+app.get('/api/persons/:id', (request, response, next) => {
+    Persons.find({ _id: request.params.id })
         .then(result => {
+            response.json(result)
+        })
+        .catch(error => next(error))
+})
 
+
+app.delete("/api/persons/:id", (request, response, next) => {
+    Persons.findByIdAndRemove(request.params.id)
+        .then(result => {
             response.status(204).end()
             console.log(result)
         })
         .catch(error => next(error))
 })
 
+
 const errorHandle = (error, request, response, next) => {
     console.error(error)
+    if (error = 'CastError') {
+        return response.status(404).send({ error: 'wrong id' })
+    }
 
     next(error)
 }
+
 
 app.use(errorHandle)
 
