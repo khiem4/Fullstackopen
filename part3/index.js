19a538ae33838c4f7a9f390e940f907d9d503ec5
khiem4 <khiemcan@gmail.com>
2022-08-28 23:12:45 +0700

    exercise 3.22

--- part3/index.js
+++ part3/index.js
@@ -1,11 +1,10 @@
 require('dotenv').config()
 
-const express = require("express")
+const express = require('express')
 const app = express()
-const morgan = require("morgan")
+const morgan = require('morgan')
 const cors = require('cors')
 const Persons = require('./models/phonebook')
-const mongoose = require('mongoose')
 
 
 
@@ -16,7 +15,7 @@ app.use(express.static('build'))
 app.use(express.json())
 
 morgan.token('body', (req, res) => {
-    return JSON.stringify(req.body)
+  return JSON.stringify(req.body)
 })
 
 app.use(morgan(`
@@ -26,79 +25,79 @@ app.use(morgan(`
 
 
 app.get('/api/persons', (request, response) => {
-    const get = Persons.find({}).then(person => {
-        response.json(person)
-    })
+  Persons.find({}).then(person => {
+    response.json(person)
+  })
 })
 
 
 app.post('/api/persons', (request, response, next) => {
-    const { name, number } = request.body
+  const { name, number } = request.body
 
-    if (!name || !number) {
-        return response.status(400).json({ error: 'Please provide a name or number' }).end()
-    }
+  if (!name || !number) {
+    return response.status(400).json({ error: 'Please provide a name or number' }).end()
+  }
 
-    const newPerson = new Persons({
-        name: name,
-        number: number,
-        date: new Date()
-    })
+  const newPerson = new Persons({
+    name: name,
+    number: number,
+    date: new Date()
+  })
 
-    newPerson.save()
-        .then(result => response.json(result))
-        .catch(error => next(error))
+  newPerson.save()
+    .then(result => response.json(result))
+    .catch(error => next(error))
 })
 
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const { name, number } = request.body
+  const { name, number } = request.body
 
-    Persons.findByIdAndUpdate(request.params.id, { name, number }, { new: true })
-        .then(updatedPerson => {
-            response.json(updatedPerson)
-        })
-        .catch(error => next(error))
+  Persons.findByIdAndUpdate(request.params.id, { name, number }, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 
-app.get('/info', (request, response) => {
-    Persons.find({})
-        .then(result => response.send(`Phonebook has info for ${result.length} people <br/>${Date()}`))
-        .catch(error => next(error))
+app.get('/info', (request, response, next) => {
+  Persons.find({})
+    .then(result => response.send(`Phonebook has info for ${result.length} people <br/>${Date()}`))
+    .catch(error => next(error))
 })
 
 
 
 app.get('/api/persons/:id', (request, response, next) => {
-    Persons.find({ _id: request.params.id })
-        .then(result => {
-            response.json(result)
-        })
-        .catch(error => next(error))
+  Persons.find({ _id: request.params.id })
+    .then(result => {
+      response.json(result)
+    })
+    .catch(error => next(error))
 })
 
 
-app.delete("/api/persons/:id", (request, response, next) => {
-    Persons.findByIdAndRemove(request.params.id)
-        .then(result => {
-            response.status(204).end()
-            console.log(result)
-        })
-        .catch(error => next(error))
+app.delete('/api/persons/:id', (request, response, next) => {
+  Persons.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+      console.log(result)
+    })
+    .catch(error => next(error))
 })
 
 
 const errorHandle = (error, request, response, next) => {
-    console.error(error)
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'wrong id' })
-    }
-    if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
+  console.error(error)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'wrong id' })
+  }
+  if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
-    next(error)
+  next(error)
 }
 
 
@@ -106,6 +105,6 @@ app.use(errorHandle)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`connect to server ${PORT}`)
+  console.log(`connect to server ${PORT}`)
 })
 
--- part3/models/phonebook.js
+++ part3/models/phonebook.js
@@ -4,44 +4,44 @@ const mongoose = require('mongoose')
 const url = process.env.MONGODB_URI
 
 mongoose.connect(url)
-    .then(() => {
-        console.log(`connect to ${url}`)
-    })
-    .catch(() => {
-        console.log(`error can't connect`)
-    })
+  .then(() => {
+    console.log(`connect to ${url}`)
+  })
+  .catch(() => {
+    console.log('error can\'t connect')
+  })
 
 
 const phoneBookSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: function (v) {
+        return /\d{2,3}-\d{6}/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number`
     },
-    number: {
-        type: String,
-        minLength: 8,
-        validate: {
-            validator: function (v) {
-                return /\d{2,3}-\d{6}/.test(v)
-            },
-            message: props => `${props.value} is not a valid phone number`
-        },
-        required: [true, 'User phone number required']
-    },
-    date: {
-        type: Date,
-        required: true
-    }
+    required: [true, 'User phone number required']
+  },
+  date: {
+    type: Date,
+    required: true
+  }
 })
 
 
 phoneBookSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = new mongoose.model('Person', phoneBookSchema)
