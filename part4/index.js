3946ee474c5fc30d552470d9a9ef60acb19d1302
khiem4 <khiemcan@gmail.com>
2022-08-30 22:03:14 +0700

    config controller

--- part4/index.js
+++ part4/index.js
@@ -1,34 +1,18 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const cors = require('cors')
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+const blogRouter = require('./controllers/bloglist')
+
 
 
 const mongoUrl = process.env.mongoUrl
 mongoose.connect(mongoUrl)
 
-app.use(cors())
+
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogRouter )
 
 const PORT = 3003
 app.listen(PORT, () => {
