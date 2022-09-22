320f55399ce3738152892c514b361b8063d17417
khiem4 <khiemcan@gmail.com>
2022-09-23 00:10:31 +0700

    exercise 5.7

--- part5/blog-list/src/components/Blog.js
+++ part5/blog-list/src/components/Blog.js
@@ -1,7 +1,35 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+
+
+const Blog = ({ blog }) => {
+  const [showBlogDetails, setShowBlogDetails] = useState(false)
+
+  const show = { display: showBlogDetails ? 'none' : '' }
+
+  const visible = () => {
+    setShowBlogDetails(!showBlogDetails)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title}
+      <button onClick={visible}>view</button>
+      <div style={show}>
+        <p>likes {blog.likes}  <button>like</button></p>
+        <p>{blog.url}</p>
+        <p>{blog.author}</p>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
