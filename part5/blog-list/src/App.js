829c22c4e240c4720ba462b0a676e824c20dc1a3
khiem4 <khiemcan@gmail.com>
2022-09-22 22:43:04 +0700

    exercise 5.6

--- part5/blog-list/src/App.js
+++ part5/blog-list/src/App.js
@@ -2,19 +2,16 @@ import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
+import BlogForm from './components/BlogForm'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
-  const [title, setTitle] = useState('')
-  const [author, setAuthor] = useState('')
-  const [url, setUrl] = useState('')
   const [errorMessage, setErrorMessage] = useState(null)
   const [successMessage, setSuccessMessage] = useState(null)
   const [createBlogVisible, setCreateBlogVisible] = useState(false)
-
 
 
   useEffect(() => {
@@ -29,6 +26,7 @@ const App = () => {
     if (loggedUser) {
       const user = JSON.parse(loggedUser)
       setUser(user)
+      blogService.setToken(user.token)
     }
   }, [])
 
@@ -61,27 +59,47 @@ const App = () => {
   }
 
 
-  const handleCreateBlog = async (event) => {
-    event.preventDefault()
-
-    const blog = {
-      title: title,
-      author: author,
-      url: url,
-      id: blogs.length + 1
-    }
+  const handleCreateBlog = async (blog) => {
 
     await blogService.create(blog)
-    setBlogs(blogs.concat(blog))
-    setTitle('')
-    setAuthor('')
-    setUrl('')
+    setBlogs(blogs.concat({
+      ...blog, id: blogs.length + 1
+    }))
     setSuccessMessage(`a new blog ${blog.title} by ${blog.author} added`)
     setTimeout(() => {
       setSuccessMessage(null)
     }, 5000)
   }
 
+  const blogList = () => {
+    const hide = { display: createBlogVisible ? 'none' : '' }
+    const show = { display: createBlogVisible ? '' : 'none' }
+
+    return (
+      <>
+        <h2>blogs</h2>
+        <h2>{successMessage}</h2>
+        {user.username} logged in
+        <button onClick={handleLogout}>logout</button>
+        {blogs.map(blog =>
+          <Blog
+            key={blog.id}
+            blog={blog}
+          />
+        )}
+
+        <h2>create new</h2>
+        <div style={hide}>
+          <button onClick={() => setCreateBlogVisible(true)}>create</button>
+        </div>
+
+        <div style={show}>
+          <BlogForm handleCreateBlog={handleCreateBlog} />
+          <button onClick={() => setCreateBlogVisible(false)}>cancel</button>
+        </div>
+      </>
+    )
+  }
 
   const loginForm = () => (
     <div>
@@ -116,65 +134,7 @@ const App = () => {
     </div>
   )
 
-  const blogList = () => {
-    const hide = { display: createBlogVisible ? 'none' : '' }
-    const show = { display: createBlogVisible ? '' : 'none' }
 
-    return (
-      <>
-        <h2>blogs</h2>
-        <h2>{successMessage}</h2>
-
-        {user.username} logged in
-        <button onClick={handleLogout}>logout</button>
-        {blogs.map(blog =>
-          <Blog
-            key={blog.id}
-            blog={blog}
-          />
-        )}
-
-        <h2>create new</h2>
-
-        <div style={hide}>
-          <button onClick={() => setCreateBlogVisible(true)}>create</button>
-        </div>
-        <div style={show}>
-          <form onSubmit={handleCreateBlog}>
-            <div>
-              title
-              <input
-                type='text'
-                value={title}
-                name='title'
-                onChange={({ target }) => setTitle(target.value)}
-              />
-
-              <div>
-                author
-                <input
-                  type='text'
-                  value={author}
-                  name='author'
-                  onChange={({ target }) => setAuthor(target.value)}
-                />
-
-              </div>
-              url
-              <input
-                type='text'
-                value={url}
-                name='url'
-                onChange={({ target }) => setUrl(target.value)}
-              />
-            </div>
-            <button>save</button>
-          </form>
-          <button onClick={() => setCreateBlogVisible(false)}>cancel</button>
-        </div>
-      </>
-    )
-  }
 
   return (
     <div>
