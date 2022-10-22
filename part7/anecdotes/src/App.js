4ef6882c7dc86305ae4b999051e5b0da30f0574f
khiem <khiemcan@gmail.com>
2022-10-23 01:38:15 +0700

    7.4

--- part7/anecdotes/src/App.js
+++ part7/anecdotes/src/App.js
@@ -1,6 +1,6 @@
-import { number } from 'prop-types'
 import { useState } from 'react'
 import { BrowserRouter, Link, Route, Routes, useParams } from 'react-router-dom'
+import { useField } from './hooks'
 
 const Menu = () => {
   const padding = {
@@ -65,17 +65,17 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
 
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
   }
@@ -86,19 +86,19 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input name={'content'} {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input name={'author'} {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
+          <input name={'info'} {...info} />
         </div>
         <button>create</button>
       </form>
-    </div>
+    </div >
   )
 
 }
@@ -180,6 +180,3 @@ const App = () => {
 
 export default App
 
-//   < Link style = { padding } to = "/" > anecdotes</Link >
-// <Link style={padding} to="/about">About</Link>
-// <Link style={padding} to="/create">create new</Link>
