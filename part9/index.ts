99be75b65e5cde709d8b1126f2ab21c5546aeed5
khiem <khiemcan@gmail.com>
2023-04-09 01:44:56 +0700

    9.6-9.7

--- part9/index.ts
+++ part9/index.ts
@@ -1,11 +1,38 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import express from 'express'
 import calculateBmi from './bmi'
 
 const app = express()
 
-app.get('/hello', (_req, res) => {
-  res.json('hello fullstack')
+app.use(express.json());
+
+app.post('/exercises', (req, res) => {
+  const { dailyExercises, target } = req.body
+  const isDailyExercises = dailyExercises.find((number: number) =>
+    isNaN(Number(number))
+  )
+
+  if (isNaN(Number(target)) || isDailyExercises) {
+    return res.json(
+      { error: "malformatted  parameters" }
+    )
+  }
+
+  return res.json(
+    {
+      "periodLength": 7,
+      "trainingDays": 4,
+      "success": false,
+      "rating": 1,
+      "ratingDescription": "bad",
+      "target": 2.5,
+      "average": 1.2142857142857142
+    }
+  )
 })
+
 
 app.get('/bmi', (req, res) => {
   const { height, weight } = req.query
@@ -13,13 +40,13 @@ app.get('/bmi', (req, res) => {
   if (!isNaN(Number(height)) && !isNaN(Number(weight))) {
     const result = calculateBmi(Number(height), Number(weight))
     return res.json(result)
-  } else {
-    return res.json({
-      error: "malformatted parameters"
-    })
   }
-})
 
+  return res.json({
+    error: "malformatted parameters"
+  })
+}
+)
 
 const PORT = 3003
 
