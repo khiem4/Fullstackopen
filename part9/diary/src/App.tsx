72eeb6d8de6c023124e2c620c922237f653926c3
khiem <khiemcan@gmail.com>
2023-05-02 01:17:29 +0700

    9.19

--- part9/diary/src/App.tsx
+++ part9/diary/src/App.tsx
@@ -8,15 +8,13 @@ export interface Diary {
   comment: string
 }
 
-export type error = string | undefined
-
 function App() {
   const [weather, setWeather] = useState('')
   const [comment, setComment] = useState('')
   const [visibility, setVisibility] = useState('')
-  const [date, setDate] = useState('')
+  const [date] = useState(new Date().toString())
   const [diaries, setDiaries] = useState<Diary[]>([])
-  const [error, setError] = useState<error>('')
+  const [error, setError] = useState('')
 
   useEffect(() => {
     (async function () {
@@ -36,6 +34,10 @@ function App() {
     }
 
     diaryService.create(newDiary, setError)
+    setTimeout(() => {
+      setError('')
+    }, 3000)
+
     setDiaries(diaries.concat(newDiary))
   }
 
@@ -48,44 +50,80 @@ function App() {
         </p>
         <form
           onSubmit={submit}
-          style={{ display: "grid", width: "30%", }} >
-          <label htmlFor="weather">weather</label>
+          style={{ display: "grid", width: "30%", }}>
+          <h4>Weather</h4>
+          <label htmlFor="weather">rainy</label>
           <input
-            type="text"
-            name='weather'
-            onChange={(e) => setWeather(e.target.value)}
-            value={weather}
+            type="radio"
+            name="weather"
+            onChange={() => setWeather('rainy')}
           />
-          <label htmlFor="comment">comment</label>
+          <label htmlFor="weather">sunny</label>
           <input
-            type="text"
-            name='comment'
-            onChange={(e) => setComment(e.target.value)}
-            value={comment}
+            type="radio"
+            name="weather"
+            onChange={() => setWeather('sunny')}
+          />
+          <label htmlFor="weather">windy</label>
+          <input
+            type="radio"
+            name="weather"
+            onChange={() => setWeather('windy')}
+          />
+          <label htmlFor="weather">cloudy</label>
+          <input
+            type="radio"
+            name="weather"
+            onChange={() => setWeather('cloudy')}
+          />
 
-          />
-          <label htmlFor="visibility">visibility</label>
+          <h4>Visibility</h4>
+          <label htmlFor="good">good</label>
           <input
-            type="text"
-            name='visibility'
-            onChange={(e) => setVisibility(e.target.value)}
-            value={visibility}
+            type="radio"
+            name="visibility"
+            onChange={() => setVisibility('good')}
           />
+          <label htmlFor="poor">ok</label>
+          <input
+            type="radio"
+            name="visibility"
+            onChange={() => setWeather('ok')}
+          />
+          <label htmlFor="poor">great</label>
+          <input
+            type="radio"
+            name="visibility"
+            onChange={() => setWeather('great')}
+          />
+          <label htmlFor="poor">poor</label>
+          <input
+            type="radio"
+            name="visibility"
+            onChange={() => setWeather('poor')}
+          />
+
           <label htmlFor="date">date</label>
           <input
-            type="text"
+            type='date'
             name='date'
-            onChange={(e) => setDate(e.target.value.toString())}
-            value={date}
           />
-          <button>add</button>
+
+          <label htmlFor="comment">Comment</label>
+          <input
+            type="text"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)} />
+          <button style={{ margin: '10px' }}>
+            add
+          </button>
         </form>
       </div >
       <div>
         <h3>diary entries</h3>
         {diaries.map((diary, index) =>
           <div key={index}>
-            <p>{diary.date}</p>
+            <p>{diary.date.toString()}</p>
             <p>visibility :{diary.visibility}</p>
             <p>weather :{diary.weather}</p>
             <p>comment :{diary.comment}</p>
