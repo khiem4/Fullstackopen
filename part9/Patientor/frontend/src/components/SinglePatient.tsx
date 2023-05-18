ce77547d7d10701f8dbe6e1c0494e3a3a5b2366a
khiem <khiemcan@gmail.com>
2023-05-18 20:13:32 +0700

    9.23

--- part9/Patientor/frontend/src/components/SinglePatient.tsx
+++ part9/Patientor/frontend/src/components/SinglePatient.tsx
@@ -7,7 +7,6 @@ import { Patient } from "../types"
 function SinglePatient() {
     const [patient, setPatient] = useState<Patient>()
     const { id } = useParams()
-    console.log('id:', id)
 
     useEffect(() => {
         const fetchOnePatient = async () => {
@@ -27,6 +26,18 @@ function SinglePatient() {
                     <p>{patient.name}</p>
                     <p>{patient.ssn}</p>
                     <p>{patient.occupation}</p>
+                    <h3>entries</h3>
+                    {patient.entries.map((v, index) =>
+                        <div key={index + 1}>
+                            <p>{v.date}</p>
+                            <p>{v.description}</p>
+                            {v.diagnosisCodes?.map((item, index) =>
+                                <ul key={index + 1}>
+                                    <li>{item}</li>
+                                </ul>
+                            )}
+                        </div>
+                    )}
                 </div>
             }
         </>
